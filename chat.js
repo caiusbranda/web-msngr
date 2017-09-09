ccad0335e8edd4a3e0a13da949e48ce7417cac1b
Caius Branda <caius.branda@gmail.com>
2017-09-09 13:29:59 -0400

    actually it wasnt ready but now is

--- chat.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var schema = new Schema({
-    users: [{type: Schema.Types.ObjectId, ref: 'User'}],
-    messages: [{type: Schema.Types.ObjectId, ref: 'Message'}]
-});
-
-module.exports = mongoose.model('User', schema);
--- message.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var schema = new Schema({
-    contentText: {type: String, required: true},
-    author: {type: Schema.Types.ObjectId, ref: 'User'}
-});
-
-module.exports = mongoose.model('Message', schema);
