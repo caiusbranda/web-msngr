b7bbcddfd5e9815b162f7578598fe57a6f849632
Jafer Haider <itsjafer@gmail.com>
2017-09-09 13:57:23 -0400

    Created nodejs backend. Created a routes folder. npm run build now compiles the angular dist folder so that npm start creates an express server using those files.

---
+++ app.js
@@ -0,0 +1,34 @@
+// initialize modules
+const express = require('express');
+const app = express();
+const http = require('http');
+const bodyParser = require('body-parser')
+const path = require('path');
+const routes = require('./server/routes');
+
+// set up body data parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended: false }))
+
+// set up our static path 
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// setting up routes
+app.use('/', routes)
+
+// redirect other requests to the index
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+})
+
+// setting up the server
+const port = process.env.PORT || '3000';
+const server = http.createServer(app)
+
+// start the server
+server.listen(port);
+console.log("Server started on localhost:" + port);
+
+
+
+
