bea6ad7fc194e805968502b6db2392d3ad1f0d36
Caius Branda <caius.branda@gmail.com>
2017-09-10 23:13:06 -0400

    FRONT-END-SETUP: Added messages within chat. (Viewing, adding, deleting)

---
+++ src/app/chats/message-list/message-list.component.ts
@@ -0,0 +1,17 @@
+import { Message } from './message/message.model';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-list',
+  templateUrl: './message-list.component.html',
+  styleUrls: ['./message-list.component.css']
+})
+export class MessageListComponent implements OnInit {
+    @Input() messages: Message[] = [];
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
